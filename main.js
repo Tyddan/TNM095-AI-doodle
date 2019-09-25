dc92f50a6dd392b928734762f652eb29155cb678
Amanda <amanda.tyden@gmail.com>
2019-09-25 09:30:48 +0200

    Pushed from aniisas

---
+++ main.js
@@ -0,0 +1,249 @@
+/*
+variables
+*/
+let model;
+const classNames = [];
+let canvas;
+let coords = [];
+let mousePressed = false;
+let mode;
+
+/*
+prepare the drawing canvas
+*/
+$(function() {
+    canvas = window.canvas = new fabric.canvas('canvas');
+    canvas.backgroundColor = '#ffffff';
+    canvas.isDrawingMode = 0;
+    canvas.freeDrawingBrush.color = "black";
+    canvas.freeDrawingBrush.width = 10;
+    canvas.renderAll();
+    //setup listeners
+    canvas.on('mouse:up', function(e) {
+        getFrame();
+        mousePressed = false;
+    });
+    canvas.on('mouse:down', function(e) {
+        mousePressed = true;
+    });
+    canvas.on('mouse:move', function(e) {
+        recordCoordinates(e)
+    });
+});
+
+/*
+set the table of the predictions
+*/
+function setTable(top5, probability) {
+    //loop over the predictions
+    for (let i = 0; i < top5.length; i++) {
+        let sym = document.getElementById('sym' + (i + 1));
+        let prob = document.getElementById('prob' + (i + 1));
+        sym.innerHTML = top5[i];
+        prob.innerHTML = Math.round(probability[i] * 100);
+    }
+    //create the pie
+    createPie(".pieID.legend", ".pieID.pie");
+
+}
+
+/*
+record the current drawing coordinates
+*/
+function recordCoordinates(event) {
+    const pointer = canvas.getPointer(event.e);
+    const posX = pointer.x;
+    const posY = pointer.y;
+
+    if (posX >= 0 && posY >= 0 && mousePressed) {
+        coords.push(pointer);
+    }
+}
+
+/*
+get the best bounding box by trimming around the drawing
+*/
+function getMinBox() {
+    //get coordinates
+    const coordinateX = coords.map(function (p) {
+        return p.x;
+    });
+    const coordinateY = coords.map(function (p) {
+        return p.y;
+    });
+
+    //find top left and bottom right corners
+    const min_coords = {
+        x: Math.min.apply(null, coordinateX),
+        y: Math.min.apply(null, coordinateY)
+    };
+    const max_coords = {
+        x: Math.max.apply(null, coordinateX),
+        y: Math.max.apply(null, coordinateY)
+    };
+
+    return {
+        min: min_coords,
+        max: max_coords
+    };
+}
+
+/*
+get the current image data
+*/
+function getImageData() {
+    //get the minimum bounding box around the drawing
+    const mbb = getMinBox();
+
+    //get image data according to dpi
+    const dpi = window.devicePixelRatio;
+    return canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
+        (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+}
+
+/*
+get the prediction
+*/
+function getFrame() {
+    //make sure we have at least two recorded coordinates
+    if (coords.length >= 2) {
+
+        //get the image data from the canvas
+        const imgData = getImageData();
+
+        //get the prediction
+        const pred = model.predict(preprocess(imgData)).dataSync();
+
+        //find the top 5 predictions
+        const indices = findIndicesOfMax(pred, 5);
+        const prob = findTopValues(pred, 5);
+        const names = getClassNames(indices);
+
+        //set the table
+        setTable(names, prob);
+    }
+
+}
+
+/*
+get the the class names
+*/
+function getClassNames(indices) {
+    const output = [];
+    for (let i = 0; i < indices.length; i++)
+        output[i] = classNames[indices[i]];
+    return output;
+}
+
+/*
+load the class names
+*/
+async function loadDict() {
+    let loc = 'model/class_names.txt';
+
+    await $.ajax({
+        url: loc,
+        dataType: 'text',
+    }).done(success);
+}
+
+/*
+load the class names
+*/
+function success(data) {
+    const lst = data.split(/\n/);
+    for (let i = 0; i < lst.length - 1; i++) {
+        classNames[i] = lst[i]
+    }
+}
+
+/*
+get indices of the top prob
+*/
+function findIndicesOfMax(inp, count) {
+    const out = [];
+    for (let i = 0; i < inp.length; i++) {
+        out.push(i); // add index to output array
+        if (out.length > count) {
+            out.sort(function(a, b) {
+                return inp[b] - inp[a];
+            }); // descending sort the output array
+            out.pop(); // remove the last index (index of smallest element in output array)
+        }
+    }
+    return out;
+}
+
+/*
+find the top 5 predictions
+*/
+function findTopValues(inp, count) {
+    const out = [];
+    let indices = findIndicesOfMax(inp, count);
+    // show 5 greatest scores
+    for (let i = 0; i < indices.length; i++)
+        out[i] = inp[indices[i]];
+    return out;
+}
+
+/*
+preprocess the data
+*/
+function preprocess(imgData) {
+    return tf.tidy(() => {
+        //convert to a tensor
+        let tensor = tf.browser.fromPixels(imgData, numChannels = 1);
+
+        //resize
+        const resize = tf.image.resizeBilinear(tensor, [28, 28]).toFloat();
+
+        //normalize
+        const offset = tf.scalar(255.0);
+        const normalized = tf.scalar(1.0).sub(resize.div(offset));
+
+        //We add a dimension to get a batch shape
+        return normalized.expandDims(0);
+    })
+}
+
+/*
+load the model
+*/
+async function start(cur_mode) {
+    //arabic or english
+    mode = cur_mode;
+
+    //load the model
+    model = await tf.loadLayersModel('model/model.json');
+
+    //warm up
+    model.predict(tf.zeros([1, 28, 28, 1]));
+
+    //allow drawing on the canvas
+    allowDrawing();
+
+    //load the class names
+    await loadDict()
+}
+
+/*
+allow drawing on canvas
+*/
+function allowDrawing() {
+    canvas.isDrawingMode = 1;
+    document.getElementById('status').innerHTML = 'Model Loaded';
+    $('button').prop('disabled', false);
+    const slider = document.getElementById('myRange');
+    slider.oninput = function() {
+        canvas.freeDrawingBrush.width = this.value;
+    };
+}
+
+/*
+clear the canvas
+*/
+function erase() {
+    canvas.clear();
+    canvas.backgroundColor = '#ffffff';
+    coords = [];
+}
---
+++ pie.js
@@ -0,0 +1,55 @@
+function sliceSize(dataNum, dataTotal) {
+    return (dataNum / dataTotal) * 360;
+}
+function addSlice(sliceSize, pieElement, offset, sliceID, color) {
+    $(pieElement).append("<div class='slice "+sliceID+"'><span></span></div>");
+    offset = offset - 1;
+    const sizeRotation = -179 + sliceSize;
+    $("."+sliceID).css({
+        "transform": "rotate("+offset+"deg) translate3d(0,0,0)"
+    });
+    $("."+sliceID+" span").css({
+        "transform"       : "rotate("+sizeRotation+"deg) translate3d(0,0,0)",
+        "background-color": color
+    });
+}
+function iterateSlices(sliceSize, pieElement, offset, dataCount, sliceCount, color) {
+    const sliceID = "s" + dataCount + "-" + sliceCount;
+    const maxSize = 179;
+    if(sliceSize<=maxSize) {
+        addSlice(sliceSize, pieElement, offset, sliceID, color);
+    } else {
+        addSlice(maxSize, pieElement, offset, sliceID, color);
+        iterateSlices(sliceSize-maxSize, pieElement, offset+maxSize, dataCount, sliceCount+1, color);
+    }
+}
+function createPie(dataElement, pieElement) {
+    let i;
+    const listData = [];
+    $(dataElement+" span").each(function() {
+        listData.push(Number($(this).html()));
+    });
+    let listTotal = 0;
+    for(i = 0; i<listData.length; i++) {
+        listTotal += listData[i];
+    }
+    let offset = 0;
+    const color = [
+        "cornflowerblue",
+        "olivedrab",
+        "orange",
+        "tomato",
+        "crimson",
+        "purple",
+        "turquoise",
+        "forestgreen",
+        "navy",
+        "gray"
+    ];
+    for(i = 0; i<listData.length; i++) {
+        const size = sliceSize(listData[i], listTotal);
+        iterateSlices(size, pieElement, offset, i, 0, color[i]);
+        $(dataElement+" li:nth-child("+(i+1)+")").css("border-color", color[i]);
+        offset += size;
+    }
+}
