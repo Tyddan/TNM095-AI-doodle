df8b63076b0b664d4aaa5c81bda3a3f2fe7a8bd8
Aniisa <anibi335@student.liu.se>
2019-09-25 12:33:26 +0200

    Update pie.js

--- pie.js
+++ pie.js
@@ -5,10 +5,10 @@ function addSlice(sliceSize, pieElement, offset, sliceID, color) {
   $(pieElement).append("<div class='slice "+sliceID+"'><span></span></div>");
   offset = offset - 1;
   const sizeRotation = -179 + sliceSize;
-  $("."+sliceID).cssRules({
+  $("."+sliceID).css({
     "transform": "rotate("+offset+"deg) translate3d(0,0,0)"
   });
-  $("."+sliceID+" span").cssRules({
+  $("."+sliceID+" span").css({
     "transform"       : "rotate("+sizeRotation+"deg) translate3d(0,0,0)",
     "background-color": color
   });
@@ -27,7 +27,7 @@ function createPie(dataElement, pieElement) {
   let i;
   const listData = [];
   $(dataElement+" span").each(function() {
-    listData.push(Number($(this).html ()));
+    listData.push(Number($(this).html()));
   });
   let listTotal = 0;
   for(i = 0; i<listData.length; i++) {
@@ -49,7 +49,7 @@ function createPie(dataElement, pieElement) {
   for(i = 0; i<listData.length; i++) {
     const size = sliceSize(listData[i], listTotal);
     iterateSlices(size, pieElement, offset, i, 0, color[i]);
-    $(dataElement+" li:nth-child("+(i+1)+")").cssRules("border-color", color[i]);
+    $(dataElement+" li:nth-child("+(i+1)+")").css("border-color", color[i]);
     offset += size;
   }
 }
