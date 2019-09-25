806c3cb57b7fa0008492f300522f847ed70a1996
Aniisa <anibi335@student.liu.se>
2019-09-25 12:36:48 +0200

    Revert "Fixing main code"
    This reverts commit a6cd15ae8f667f011706e6a8e9b3fc40a5c56c86.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 variables
 */
 var model;
+var canvas;
 var classNames = [];
 var canvas;
 var coords = [];
@@ -19,28 +20,28 @@ $(function() {
     canvas.freeDrawingBrush.width = 10;
     canvas.renderAll();
     //setup listeners 
-    canvas.on('mouse:up', function() {
+    canvas.on('mouse:up', function(e) {
         getFrame();
         mousePressed = false
     });
-    canvas.on('mouse:down', function() {
+    canvas.on('mouse:down', function(e) {
         mousePressed = true
     });
     canvas.on('mouse:move', function(e) {
-        recordCoordinates(e)
+        recordCoor(e)
     });
-});
+})
 
 /*
 set the table of the predictions 
 */
-function setTable(top5, probability) {
+function setTable(top5, probs) {
     //loop over the predictions 
     for (var i = 0; i < top5.length; i++) {
-        let sym = document.getElementById('sym' + (i + 1));
-        let prob = document.getElementById('prob' + (i + 1));
-        sym.innerHTML = top5[i];
-        prob.innerHTML = Math.round(probability[i] * 100);
+        let sym = document.getElementById('sym' + (i + 1))
+        let prob = document.getElementById('prob' + (i + 1))
+        sym.innerHTML = top5[i]
+        prob.innerHTML = Math.round(probs[i] * 100)
     }
     //create the pie 
     createPie(".pieID.legend", ".pieID.pie");
@@ -50,7 +51,7 @@ function setTable(top5, probability) {
 /*
 record the current drawing coordinates
 */
-function recordCoordinates(event) {
+function recordCoor(event) {
     var pointer = canvas.getPointer(event.e);
     var posX = pointer.x;
     var posY = pointer.y;
@@ -65,23 +66,24 @@ get the best bounding box by trimming around the drawing
 */
 function getMinBox() {
     //get coordinates 
-    var coordinateX = coords.map(function(p) {
+    var coorX = coords.map(function(p) {
         return p.x
     });
-    var coordinateY = coords.map(function(p) {
+    var coorY = coords.map(function(p) {
         return p.y
     });
 
     //find top left and bottom right corners 
     var min_coords = {
-        x: Math.min.apply(null, coordinateX),
-        y: Math.min.apply(null, coordinateY)
-    };
+        x: Math.min.apply(null, coorX),
+        y: Math.min.apply(null, coorY)
+    }
     var max_coords = {
-        x: Math.max.apply(null, coordinateX),
-        y: Math.max.apply(null, coordinateY)
-    };
+        x: Math.max.apply(null, coorX),
+        y: Math.max.apply(null, coorY)
+    }
 
+    //return as strucut 
     return {
         min: min_coords,
         max: max_coords
@@ -93,12 +95,13 @@ get the current image data
 */
 function getImageData() {
         //get the minimum bounding box around the drawing 
-        const mbb = getMinBox();
+        const mbb = getMinBox()
 
         //get image data according to dpi 
-        const dpi = window.devicePixelRatio;
-        return canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
-            (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+        const dpi = window.devicePixelRatio
+        const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
+                                                      (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+        return imgData
     }
 
 /*
@@ -109,18 +112,18 @@ function getFrame() {
     if (coords.length >= 2) {
 
         //get the image data from the canvas 
-        const imgData = getImageData();
+        const imgData = getImageData()
 
         //get the prediction 
-        const pred = model.predict(preprocess(imgData)).dataSync();
+        const pred = model.predict(preprocess(imgData)).dataSync()
 
         //find the top 5 predictions 
-        const indices = findIndicesOfMax(pred, 5);
-        const probability = findTopValues(pred, 5);
-        const names = getClassNames(indices);
+        const indices = findIndicesOfMax(pred, 5)
+        const probs = findTopValues(pred, 5)
+        const names = getClassNames(indices)
 
         //set the table 
-        setTable(names, probability)
+        setTable(names, probs)
     }
 
 }
@@ -129,10 +132,10 @@ function getFrame() {
 get the the class names 
 */
 function getClassNames(indices) {
-    var output = [];
+    var outp = []
     for (var i = 0; i < indices.length; i++)
-        output[i] = classNames[indices[i]];
-    return output
+        outp[i] = classNames[indices[i]]
+    return outp
 }
 
 /*
@@ -140,8 +143,10 @@ load the class names
 */
 async function loadDict() {
 
+    loc = 'model/class_names.txt'
+    
     await $.ajax({
-        url: 'model/class_names.txt',
+        url: loc,
         dataType: 'text',
     }).done(success);
 }
@@ -150,39 +155,40 @@ async function loadDict() {
 load the class names
 */
 function success(data) {
-    const lst = data.split(/\n/);
+    const lst = data.split(/\n/)
     for (var i = 0; i < lst.length - 1; i++) {
-        classNames[i] = lst[i]
+        let symbol = lst[i]
+        classNames[i] = symbol
     }
 }
 
 /*
-get indices of the top probabilities
+get indices of the top probs
 */
 function findIndicesOfMax(inp, count) {
-    var out = [];
+    var outp = [];
     for (var i = 0; i < inp.length; i++) {
-        out.push(i); // add index to output array
-        if (out.length > count) {
-            out.sort(function(a, b) {
+        outp.push(i); // add index to output array
+        if (outp.length > count) {
+            outp.sort(function(a, b) {
                 return inp[b] - inp[a];
             }); // descending sort the output array
-            out.pop(); // remove the last index (index of smallest element in output array)
+            outp.pop(); // remove the last index (index of smallest element in output array)
         }
     }
-    return out;
+    return outp;
 }
 
 /*
 find the top 5 predictions
 */
 function findTopValues(inp, count) {
-    var out = [];
-    let indices = findIndicesOfMax(inp, count);
+    var outp = [];
+    let indices = findIndicesOfMax(inp, count)
     // show 5 greatest scores
     for (var i = 0; i < indices.length; i++)
-        out[i] = inp[indices[i]];
-    return out
+        outp[i] = inp[indices[i]]
+    return outp
 }
 
 /*
@@ -191,18 +197,18 @@ preprocess the data
 function preprocess(imgData) {
     return tf.tidy(() => {
         //convert to a tensor 
-        let tensor = tf.browser.fromPixels(imgData, numChannels = 1);
+        let tensor = tf.browser.fromPixels(imgData, numChannels = 1)
         
         //resize 
-        const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat();
+        const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
         
         //normalize 
         const offset = tf.scalar(255.0);
         const normalized = tf.scalar(1.0).sub(resized.div(offset));
 
         //We add a dimension to get a batch shape 
-
-        return normalized.expandDims(0)
+        const batched = normalized.expandDims(0)
+        return batched
     })
 }
 
@@ -213,13 +219,13 @@ async function start() {
     
     
     //load the model 
-    model = await tf.loadLayersModel('model/model.json');
+    model = await tf.loadLayersModel('model/model.json')
     
     //warm up 
-    model.predict(tf.zeros([1, 28, 28, 1]));
+    model.predict(tf.zeros([1, 28, 28, 1]))
     
     //allow drawing on the canvas 
-    allowDrawing();
+    allowDrawing()
     
     //load the class names
     await loadDict()
@@ -241,7 +247,7 @@ function allowDrawing() {
 }
 
 /*
-clear the canvas
+clear the canvs 
 */
 function erase() {
     canvas.clear();
